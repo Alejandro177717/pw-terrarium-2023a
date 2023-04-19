f5e740b4aeb21f03262bafd14e7d7f14e2ad284f
Alejandro177717 <107718271+Alejandro177717@users.noreply.github.com>
2023-04-19 18:31:28 +0000

    Controlando elementos

---
+++ script.js
@@ -0,0 +1,11 @@
+//Tomando el control del objeto
+let plant1 = document.getElementById("plant1")
+
+//
+plant1.addEventListener("click", ()=>{
+    let name = prompt("Hola, ¿Cual es tu nombre?");
+    if(name !== ""){
+    alert(`${name} please, SAVE THE PLANET!!!`);
+    }
+});
+
