7945178d9b1dcb88428a5581e0c892f5fcde55bf
Alejandro177717 <107718271+Alejandro177717@users.noreply.github.com>
2023-04-19 19:05:53 +0000

    Building dragging function

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-//Tomando el control del objeto
-let plant1 = document.getElementById("plant1")
+//Haciendo que la planta 1 de mi documento 
+//sea arrastable.
+dragElement(document.getElementById("plant1"))
 
-//
-plant1.addEventListener("click", ()=>{
-    let name = prompt("Hola, ¿Cual es tu nombre?");
-    if(name !== ""){
-    alert(`${name} please, SAVE THE PLANET!!!`);
+//Implementando la funcion drag element
+function dragElement( terrariumElement ){
+    // Creando variables que controlaran las posiciones
+    // Iniciales y finales
+    let pos1 = 0, 
+    pos2 = 0,
+    pos3 = 0, 
+    pos4 = 0
+    //Registrando un evento
+    terrariumElement.onpointerdown = pointerDrag
+        
+    
+    function pointerDrag( event ){
+        //Previene todo comportamiento
+        //que tenga por defecto
+        // el evento en cuestion en 
+        // mi documento
+        event.preventDefault();
+        console.log(" + On pointer down");
     }
-});
-
+}
